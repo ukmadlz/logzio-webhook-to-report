b1dcd017532986d02cc94452e8d493e0bf1cfaa2
Mike Elsmore <mike.elsmore@logz.io>
2020-05-12 14:58:35 +0100

    Remove the defunct test

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -1,7 +1,6 @@
 import * as Hapi from '@hapi/hapi';
 import Server from '../src/server';
 
-test('Server default HOST & PORT', () => {
+test('Server default PORT', () => {
   expect(Server.info.port).toBe(3000);
-  expect(Server.info.host).toBe('localhost');
 });
